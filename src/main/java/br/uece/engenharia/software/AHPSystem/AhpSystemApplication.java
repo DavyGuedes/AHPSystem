4d217178e86948911b016849287d093a73f71968
Marcos Davy Guedes Dias <m.davy.br@gmail.com>
2018-05-31 19:22:37 -0300

    Criacao do projeto

---
+++ src/main/java/br/uece/engenharia/software/AHPSystem/AhpSystemApplication.java
@@ -0,0 +1,12 @@
+package br.uece.engenharia.software.AHPSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhpSystemApplication.class, args);
+	}
+}
